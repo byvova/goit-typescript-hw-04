6a82a9577882698f2862762316a6a35ee52395f6
byvova <133043645+byvova@users.noreply.github.com>
2024-04-11 20:21:47 +0300

    somesthing wrong with h3 hw 1-2 done

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,4 +1,15 @@
-import React, {useReducer} from "react";
+import React, { useReducer } from "react";
+
+type RequestStep = 'start' | 'pending' | 'finished' | 'idle';
+
+type State = {
+  isRequestInProgress: boolean;
+  requestStep: RequestStep;
+}
+
+type Action = {
+  type: 'START_REQUEST' | 'PENDING_REQUEST' | 'FINISH_REQUEST' | 'RESET_REQUEST'
+}
 
 const initialState: State = {
   isRequestInProgress: false,
